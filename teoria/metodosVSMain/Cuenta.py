401ee7e7a5633a2368238665306aa6e2cce039d3
Ricardo Nava Ramírez <richard2911@ciencias.unam.mx>
2026-03-08 21:54:28 -0600

    Add files via upload

---
+++ teoria/metodosVSMain/Cuenta.py
@@ -0,0 +1,22 @@
+''' 
+Creado en Febrero del 2026 
+@autor: Ricardo-NR
+
+'''
+class Cuenta:
+
+	# por ahora nuestra clase sólo tiene un atributo.
+	def __init__(self, valor, tipo):
+		self.cantidad = valor
+		self.tipo = tipo
+
+	#informacion
+	def imprimirDetalles(self):
+		print(self.cantidad)
+		print(self.tipo)
+
+	def retirar(self, cantidad):
+		self.cantidad = self.cantidad - cantidad 
+
+	def depositar(self, cantidad):
+		self.cantidad = self.cantidad +cantidad 
