6af31b65e020df4866fa4de9962d11f2db364a00
Ricardo Nava Ramírez <richard2911@ciencias.unam.mx>
2026-03-24 11:16:55 -0600

    Menu.py

--- proyecto/Menu.py
+++ proyecto/Menu.py
@@ -1,6 +1,6 @@
 ''' 
 Creado en Marzo del 2026
-@autor: Ricardo-NR 
+@author: Ricardo-NR
 '''
 
 from Auto import *
@@ -9,13 +9,34 @@ class Menu:
 
 	def __init__(self, valor):
 		self.mensajeDeBienvenida = valor
-		
+		self.cargaDatos()
+
+	def cargaDatos(self):
+		self.autos = [Auto("Toyota", "Corolla", 2020),
+			Auto("Mercedes-Benz", "Clase G", 2025)]
+
 	def darBienvenida(self):
 		print(self.mensajeDeBienvenida)
 
 	def despliegaMenu(self):
 		print("Las opciones son:")
-		print("1. Mostrar los autos guardados")
-		print("2. Autos disponibles")
-		opcion = input("Teclea la opcion:")
+		print("1. Ver modelos disponibles")
+		print("2. Salir")
+		opcion = input("Tecleaste la opcion:")
+		return opcion
+		
+	def procesaOpcion(self, opcion):
+		if(opcion == "1"):
+			print("Estos son los autos disponibles:")
+			for auto in self.autos:
+				print(auto)
+
+		elif(opcion == "2"):
+			print("Saliendo del sistema...")
+			return False
+		
+		else:
+			print("Opción invalido")
+
+		return True
 		
