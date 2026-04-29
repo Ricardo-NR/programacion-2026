648de1c355094b4e3f1bc93b1aec37896f34c1fe
Ricardo Nava Ramírez <richard2911@ciencias.unam.mx>
2026-04-28 21:41:52 -0600

    Add files via upload

---
+++ teoria/objetosComoCadenas/Cliente.py
@@ -0,0 +1,21 @@
+''' 
+Creado en Abril del 2026 
+@autor: Ricardo-NR
+
+''' 
+
+from Cuenta import *
+
+class Cliente :
+	def __init__( self, n, d, e, c ) :
+		self.nombre = n
+		self.direccion = d
+		self.edad = e
+		self.cuenta = c
+
+	def __str__( self ):
+		tmp = "Nombre::" + str( self.nombre )
+		tmp += "\nDireccion::" + str( self.direccion )
+		tmp += "\nEdad::" + str( self.edad )
+		tmp += "\nLa cuenta::" + str( self.cuenta )
+		return tmp
---
+++ teoria/objetosComoCadenas/Main.py
@@ -0,0 +1,24 @@
+''' 
+Creado en Abril del 2026 
+@autor: Ricardo-NR
+''' 
+
+from Cuenta import *
+from Cliente import *
+
+class Main:
+	pass
+print ( "Desde las pruebas" )
+cuenta1 = Cuenta( 300 )
+print ( cuenta1 )
+cuenta1.depositar( 400 )
+print ( cuenta1 )
+
+"""
+Imprimimos un objeto y nos mandará una referencia en la memoria
+si es que no tenemos reescrito el me´todo __str__
+"""
+print ("Objeto cuenta::", cuenta1)
+
+cliente = Cliente( "Virginia", "direccion", 25, cuenta1)
+print ("Objeto cliente::", cliente )
