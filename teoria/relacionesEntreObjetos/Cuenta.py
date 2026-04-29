3b91caf27af08d840fc6753e9efd839d17a69202
Ricardo Nava Ramírez <richard2911@ciencias.unam.mx>
2026-04-28 20:49:59 -0600

    Add files via upload

---
+++ teoria/relacionesEntreObjetos/Cuenta.py
@@ -0,0 +1,18 @@
+''' 
+Creado en Marzo del 2026 
+@autor: Ricardo-NR
+
+'''
+class Cuenta:
+
+	def __init__(self, valor):
+		self.cantidad = valor
+
+	def depositar(self, valor):
+		if valor > 0:
+			self.cantidad = self.cantidad + valor
+		else:
+			print ("El valor para depositar es erroneo::")
+
+	def mostrarDetalles(self):
+		print ("La cantidad de la cuenta::", self.cantidad)
